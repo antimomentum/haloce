e0a601f1211b4890cc5ea7011e165e8d6061fb17
root <ryanb214598@hotmail.com>
2020-12-02 14:52:59 +0000

    loop fix

--- awkloop.sh
+++ awkloop.sh
@@ -1,5 +1,5 @@
 while true
 do
- ipset add LEGIT $(awk 'END{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}' output.txt)
+ ipset add LEGIT $(awk 'END{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}' output.txt) -exist
  sleep 0.018
 done
