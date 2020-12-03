9192857ae67209ae7d3b4f8e7ba7ea80f3fc98e4
root <ryanb214598@hotmail.com>
2020-12-03 11:28:13 +0000

    duplicate check removed, ipset's -switch already checks

--- iplog.lua
+++ iplog.lua
@@ -1,3 +1,4 @@
+
 api_version = "1.12.0.0"
 local path = "sapp\\output.txt"
 local output = "%ip%"
@@ -10,20 +11,9 @@ function GetIP(Ply)
     local file = assert(io.open(path, "a+"))
     if (file) then
         local IP = get_var(Ply, "$ip"):match("(%d+.%d+.%d+.%d+)")
-        if (not ContainsDuplicate(IP)) then
-            file:write(string.gsub(output, "%%ip%%", IP) .. "\n")
-        end
+        file:write(string.gsub(output, "%%ip%%", IP) .. "\n")
         io.close(file)
     end
-end
-
-function ContainsDuplicate(IP)
-    for line in io.lines(path) do
-        if (IP == line) then
-            return true
-        end
-    end
-    return false
 end
 
 function OnScriptUnload()
