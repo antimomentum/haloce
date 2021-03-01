255c292b74213deacc998e6952d8807dc6dd77fc
root <ryanb214598@hotmail.com>
2021-03-01 21:36:01 +0000

    firewall fixed

--- tc.sh
+++ tc.sh
@@ -1,3 +1,3 @@
 tc filter add dev eth0 parent ffff: u32 match ip sport 53 0xffff action drop
 iptables -t raw -D PREROUTING -i eth0 -m set --match-set DNS src -j ACCEPT
-ipset destory DNS
+ipset destroy DNS
