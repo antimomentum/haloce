bb076bc49e3ca0524d1fdf928a6301ab86c2e5f9
root <ryanb214598@hotmail.com>
2021-08-22 01:12:24 +0000

    installs Wine in Alpine-3.12

--- Alpine64bit-3.12-Wine32bitinstaller.sh
+++ Alpine64bit-3.12-Wine32bitinstaller.sh
@@ -1,5 +1,5 @@
 # Installs 32bit Wine on 64bit Alpine3.12
-# NOTE: The first command makes apk install 32bit packages only, you made need to change it back for other installations!
+# NOTE: The first command makes apk install 32bit packages only, you may need to change it back for other installations!
 
 echo "x86" > /etc/apk/arch
 wait
