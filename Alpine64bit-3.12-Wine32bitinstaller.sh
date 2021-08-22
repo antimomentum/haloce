475800dade25a4818d330df06ce2cdb16926f3fa
root <ryanb214598@hotmail.com>
2021-08-22 01:09:52 +0000

    installs Wine in Alpine-3.12

---
+++ Alpine64bit-3.12-Wine32bitinstaller.sh
@@ -0,0 +1,8 @@
+# Installs 32bit Wine on 64bit Alpine3.12
+# NOTE: The first command makes apk install 32bit packages only, you made need to change it back for other installations!
+
+echo "x86" > /etc/apk/arch
+wait
+apk add --no-cache libcurl
+wait
+apk add --no-cache wine freetype ncurses
