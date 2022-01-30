70a121835686b31482283617d5738a4578ca61d9
root <ryanb214598@hotmail.com>
2022-01-30 18:24:43 +0000

    Chimera direct-join fix

--- firewall.sh
+++ firewall.sh
@@ -13,19 +13,19 @@
 
 # Rebooting will flush ALL rules/changes below
 wait
+sysctl -w net.ipv6.conf.all.disable_ipv6=1 
+sysctl -w net.ipv6.conf.default.disable_ipv6=1 
+sysctl -w net.ipv6.conf.lo.disable_ipv6=1
 sysctl -w net.ipv4.ipfrag_low_thresh=0
 sysctl -w net.ipv4.ipfrag_high_thresh=0
 sysctl -w net.ipv4.ipfrag_time=0
-sysctl -w net.ipv6.conf.all.disable_ipv6=1 
-sysctl -w net.ipv6.conf.default.disable_ipv6=1 
-sysctl -w net.ipv6.conf.lo.disable_ipv6=1 
 # sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.core.netdev_max_backlog=4000
 wait
 sleep 1
 ipset create LEGIT hash:ip,port timeout 10
 ipset create TEST2 hash:ip timeout 30
-ipset create TEST1 hash:ip timeout 80
+ipset create TEST1 hash:ip timeout 60
 ipset create MDNS hash:ip
 wait
 ipset add MDNS 54.82.252.156
@@ -36,13 +36,17 @@ iptables -t raw -N pcheck
 iptables -t raw -N madmins
 iptables -t mangle -N reconnect
 iptables -t raw -A PREROUTING -i eth0 -m set --match-set LEGIT src,src -j ACCEPT
-iptables -t raw -A PREROUTING -i eth0 -m set --match-set MDNS src -j madmins
-iptables -t raw -A PREROUTING -i eth0 -m length --length 48 -m u32 --u32 "35=0x0a010308" -j pcheck
 iptables -t raw -A PREROUTING -i eth0 -m set --match-set TEST1 src -j pcheck
+iptables -t raw -A PREROUTING -i eth0 -m set --match-set MDNS src -j madmins
+iptables -t raw -A PREROUTING -i eth0 ! -p udp -j DROP
+iptables -t raw -A PREROUTING -i eth0 -m length --length 67 -m u32 --u32 "28=0xfefe0100" -j ctest2
+iptables -t raw -A PREROUTING -i eth0 -m length ! --length 34:48 -j DROP
+iptables -t raw -A PREROUTING -i eth0 -m length --length 48 -m u32 --u32 "35=0x0a010308" -j pcheck
 iptables -t raw -A PREROUTING -i eth0 -m length ! --length 34 -j DROP
 iptables -t raw -A PREROUTING -i eth0 -m u32 ! --u32 "28=0x5C717565" -j DROP
 iptables -t raw -A PREROUTING -i eth0 -j ctest2
 iptables -t raw -A pcheck -p udp --sport 0 -j DROP
+iptables -t raw -A pcheck ! -p udp -j DROP
 iptables -t raw -A pcheck -j SET --exist --add-set TEST1 src
 iptables -t raw -A pcheck -m u32 --u32 "42=0x1333360c" -j ACCEPT
 iptables -t raw -A pcheck -m set --match-set TEST2 src -j ctest2
@@ -51,6 +55,7 @@ iptables -t raw -A pcheck -m u32 ! --u32 "34&0xFFFFFF=0xFFFFFF" -j DROP
 iptables -t raw -A pcheck -j SET --exist --add-set TEST2 src
 iptables -t raw -A pcheck -j ACCEPT
 iptables -t raw -A ctest2 -p udp --sport 0 -j DROP
+iptables -t raw -A ctest2 ! -p udp -j DROP
 iptables -t raw -A ctest2 -j SET --exist --add-set TEST1 src
 iptables -t raw -A ctest2 -j SET --exist --add-set TEST2 src
 iptables -t raw -A ctest2 -m u32 --u32 "28=0xfefe0100" -j SET --exist --add-set LEGIT src,src
