b4115767b5771f784487415a345aaef0cb5bb848
root <ryanb214598@hotmail.com>
2022-02-06 00:07:33 +0000

    non-interfere (hopefully) flush

--- firewalls/vpngateway/flush.sh
+++ firewalls/vpngateway/flush.sh
@@ -1,5 +1,7 @@
-iptables -t nat -F
-iptables -t nat -X
+iptables -t nat -D PREROUTING -i eth0 -m udp -p udp --dport 2302 -j DNAT --to-destination 10.0.0.2:2302
+iptables -t nat -D PREROUTING -i eth0 -m udp -p udp --dport 2304 -j DNAT --to-destination 10.0.0.4:2304
+# iptables -t nat -F
+# iptables -t nat -X
 iptables -t mangle -F
 iptables -t mangle -X
 iptables -t raw -F
