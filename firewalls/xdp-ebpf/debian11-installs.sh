27854b67d4f64b1ba5f4d8b96ea96feb39dc4854
root <ryanb214598@hotmail.com>
2022-03-15 01:41:47 +0000

    comment info

--- firewalls/xdp-ebpf/debian11-installs.sh
+++ firewalls/xdp-ebpf/debian11-installs.sh
@@ -14,4 +14,9 @@ wait
 make
 wait
 sleep 1
+
+# The next line would apply the xdp rules to eth0
 # ip link set dev eth0 xdp obj xdp-drop-ebpf.o
+
+# This line would remove them:
+# ip link set dev eth0 xdp off
