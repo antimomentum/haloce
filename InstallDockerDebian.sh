b91733d8169fc3702e2f5b51d7b6acdb7d200a79
root <ryanb214598@hotmail.com>
2022-06-25 11:26:24 +0000

    start example

--- InstallDockerDebian.sh
+++ InstallDockerDebian.sh
@@ -1,8 +1,4 @@
 ## This is a quick copy pasta file for the docker installation on Debian 9 on amd64 systems
-## Update to also build a local "wineconsole/lite" container
-## And unzip
-## Creates unrequired start-example.sh script, replace "halopull" files with your own files :)
-
 
 apt-get update
 wait
@@ -29,38 +25,3 @@ add-apt-repository \
 apt-get update
 wait
 apt-get install docker-ce docker-ce-cli containerd.io
-wait
-sleep 3
-echo "Building your wineconsole/lite container"
-
-sleep 5
-
-# Create Dockerfile
-
-cat <<DOCK >Dockerfile
-FROM i386/alpine:3.13
-RUN apk add --no-cache wine freetype ncurses
-WORKDIR /game
-CMD wineconsole --backend=curses haloceded -path . -port \${INTERNAL_PORT}
-DOCK
-
-wait
-
-# Build fresh wineconsole container :)
-
-docker build -t wineconsole/lite .
-
-# Create wineconsole/lite start-example.sh script:
-
-cat <<WEND >start-example.sh
-#!/bin/bash
-# Expects a halo server directory named "halopull":
-wget -O halopull.zip https://github.com/antimomentum/halopull/archive/refs/heads/master.zip && unzip halopull.zip && mv halopull-master halopull
-wait 
-i=2302
-Here=\$(pwd)
-wait
-docker run -it -v \$Here/halopull:/game -e INTERNAL_PORT=\$i -p \$i:\$i/udp --add-host=s1.master.hosthpc.com:34.197.71.170 --add-host=hosthpc.com:34.197.71.170 wineconsole/lite
-WEND
-
-chmod +x start-example.sh
---
+++ newstart.sh
@@ -0,0 +1,39 @@
+# This newstart.sh file should only be run once if ever. Here's what it does:
+
+# 1. It installs unzip and builds a fresh wineconsole container:
+
+apt install unzip -y
+wait
+cat <<DOCK >Dockerfile
+FROM i386/alpine:3.13
+RUN apk add --no-cache wine freetype ncurses
+DOCK
+
+docker build -t wineconsole/lite .
+wait
+
+# 2. It pulls working halo files and saves them into a directory named "halopull"
+
+wget -O halopull.zip https://github.com/antimomentum/halopull/archive/refs/heads/master.zip && \\
+unzip halopull.zip && mv halopull-master halopull && rm halopull.zip
+
+
+# 3. It creates a start example script. This allows you to customize the docker run AND haloceded.exe command more personally :)
+
+cat <<DRUN >start-example.sh
+HPORT=2310 && \\
+docker run -it \\
+-v \$(pwd)/halopull:/game \\
+-w /game \\
+-e INTERNAL_PORT=\$HPORT \\
+-p \$HPORT:\$HPORT/udp \\
+wineconsole/lite \\
+wineconsole --backend=curses haloceded.exe -path . -port \$HPORT
+DRUN
+
+chmod +x start-example.sh
+
+# 4. Finally it attempts the docker run command and removes itself (newstart.sh) since it should only be once as mentioned.
+# start-example can be run any number of times
+
+./start-example.sh && rm newstart.sh
