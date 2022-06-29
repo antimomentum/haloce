09e8cb18ae17d032709387ab4e7c00d66db39e7a
root <ryanb214598@hotmail.com>
2022-06-29 01:02:59 +0000

    less layers

--- wine/Alpine64bit-3.12-Wine32bitinstaller.sh
+++ wine/Alpine64bit-3.12-Wine32bitinstaller.sh
@@ -1,8 +1,6 @@
 # Installs 32bit Wine on 64bit Alpine3.12
 # NOTE: The first command makes apk install 32bit packages only, you may need to change it back for other installations!
 
-echo "x86" > /etc/apk/arch
-wait
-apk add --no-cache libcurl
-wait
-apk add --no-cache wine freetype ncurses
+echo "x86" > /etc/apk/arch && \
+     apk add --no-cache libcurl && \
+     apk add --no-cache wine freetype ncurses
--- wine/Debian9wineinstall.sh
+++ wine/Debian9wineinstall.sh
@@ -1,11 +1,4 @@
-echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list
-apt-get update
-wait
-apt-get install -y apt-transport-https
-wait
-apt-get install -y wget && apt-get install -y && dpkg --add-architecture i386
-wait
-apt-get update
-wait
-apt install -y wine wine32 wine64 libwine libwine:i386 fonts-wine
-wait
+echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list && \
+     dpkg --add-architecture i386 && \
+     apt-get update && \
+     apt install -y wine32 libwine:i386 fonts-wine
