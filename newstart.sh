a7ff632cbb9bd8915bf81dc2eec0e657bf0cc368
root <ryanb214598@hotmail.com>
2022-09-21 08:14:59 +0000

    less

--- newstart.sh
+++ newstart.sh
@@ -1,25 +1,17 @@
-# This newstart.sh file should only be run once if ever. It assumes Docker is installed.
+# wineconsole builder
+# This script does three things, it does not run automatically
+# requires cat, chmod, and docker
 
-# Here's what it does:
-
-# 1. It installs unzip and builds a fresh wineconsole container:
-
-
-apt install unzip -y
-wait
+# 1. Creates a local wineconsole container Dockerfile:
 cat <<DOCK >Dockerfile
 FROM i386/alpine:3.13
 RUN apk add --no-cache wine freetype ncurses
+FROM scratch
+COPY --from=0 / /
 DOCK
 
+# 2. Builds from the local Dockerfile
 docker build -t wineconsole/lite .
-wait
-
-# 2. It pulls working halo files and saves them into a directory named "halopull"
-
-wget -O halopull.zip https://github.com/antimomentum/halopull/archive/refs/heads/master.zip && \\
-unzip halopull.zip && mv halopull-master halopull && rm halopull.zip
-
 
 # 3. It creates a start example script. This allows you to customize the docker run AND haloceded.exe command more personally :)
 
@@ -37,8 +29,3 @@ wineconsole --backend=curses haloceded.exe -path . -port \$HPORT
 DRUN
 
 chmod +x start-example.sh
-
-# 4. Finally it attempts the docker run command and removes itself (newstart.sh) since it should only be run once as mentioned.
-# start-example can be run any number of times
-
-./start-example.sh && rm newstart.sh
